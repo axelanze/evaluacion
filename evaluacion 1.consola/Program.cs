c491f474139fba197e7125606195e13a8518c34c
estudiante <estudiante@DESKTOP-VD55O1L>
2024-08-19 17:44:23 -0300

    Agregar archivos de proyecto.

---
+++ evaluacion 1.consola/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace evaluacion_1.consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
